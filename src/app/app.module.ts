ec0a654807fcc3f199a246c9ae4a6172a09fa865
Danman05 <danielspurrell@hotmail.com>
2023-09-06 20:08:31 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table'
-import { MatPaginatorModule } from '@angular/material/paginator';
-
-import { provideAnimations } from '@angular/platform-browser/animations';
-
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { SearchComponent } from './home-page/search/search.component';
 import { MessageContentComponent } from './home-page/message-content/message-content.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HttpClientModule } from '@angular/common/http';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { FormsModule } from '@angular/forms';
 import { UserResultComponent } from './home-page/search/user-result/user-result.component';
 import { PostFormComponent } from './home-page/message-content/post-form/post-form.component';
 import { EditProfileDialogComponent } from './profile-page/edit-profile-dialog/edit-profile-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MessagesComponent } from './home-page/message-content/messages/messages.component';
 import { AdminComponent } from './admin/admin.component';
 import { AdminBoardComponent } from './admin/admin-board/admin-board.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,18 +34,16 @@ import { AdminBoardComponent } from './admin/admin-board/admin-board.component';
     EditProfileDialogComponent,
     MessagesComponent,
     AdminComponent,
-    AdminBoardComponent
+    AdminBoardComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    MatDialogModule,
-    MatTableModule,
-    MatPaginatorModule
+    MatDialogModule
   ],
-  providers: [provideAnimations()],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
